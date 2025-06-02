b8fcd88312141d4769207b9e5509164ced565af0
rorong <rordev@ongraph.com>
2025-06-02 23:22:40 +0530

    Pass test for sum of two comma-separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,8 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.to_i
+    return numbers.to_i unless numbers.include?(",")
+
+    numbers.split(",").map(&:to_i).sum
   end
 end
