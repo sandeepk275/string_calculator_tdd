0b3479bc6149dbf91f1555645dbd472f5575236c
rorong <rordev@ongraph.com>
2025-06-02 23:11:19 +0530

    Add failing test for single number input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe StringCalculator do
   it 'returns 0 for an empty string' do
     expect(StringCalculator.new.add("")).to eq(0)
   end
+
+  it 'returns number itself for a single number' do
+    expect(StringCalculator.new.add("5")).to eq(5)
+  end
 end
