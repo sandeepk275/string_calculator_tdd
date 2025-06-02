5c883a63f9011974d86bce1bd3d8729646147823
rorong <rordev@ongraph.com>
2025-06-02 23:54:58 +0530

    Add failing test for custom delimiter support

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -20,4 +20,8 @@ RSpec.describe StringCalculator do
   it 'handles new lines between numbers' do
     expect(StringCalculator.new.add("1\n2,3")).to eq(6)
   end
+
+  it 'supports custom delimiter' do
+    expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+  end
 end
