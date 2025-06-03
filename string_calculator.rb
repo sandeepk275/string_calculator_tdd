0557bdd9ca4a0cce9674d01d22350e9cbaaa753c
rorong <rordev@ongraph.com>
2025-06-03 13:03:27 +0530

    Pass test for custom delimiter support

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,14 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i unless numbers.include?(",")
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2])
+      numbers = parts[1]
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i).sum
   end
 end
